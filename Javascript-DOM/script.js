e309a3d4b8c6d67973a217a880c9e4a1373847f3
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-09-22 11:17:33 -0300

    Atualização nos caminhos dos arquivos

--- Javascript-DOM/script.js
+++ Javascript-DOM/script.js
@@ -55,7 +55,7 @@ function alterarContexto(contexto) {
     });
 
     html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `./imagens/${contexto}.png`);
+    banner.setAttribute('src', `./Fokus-projeto/imagens/${contexto}.png`);
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `
@@ -101,13 +101,13 @@ function iniciarOuPausar() {
     audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
     iniciarOuPausarBtn.textContent = "Pausar";
-    iniciarOuPausarImg.setAttribute('src', './imagens/pause.png');
+    iniciarOuPausarImg.setAttribute('src', './Fokus-projeto/imagens/pause.png');
 }
 
 function zerar() {
     clearInterval(intervaloId);
     iniciarOuPausarBtn.textContent = "Começar";
-    iniciarOuPausarImg.setAttribute('src', './imagens/play_arrow.png');
+    iniciarOuPausarImg.setAttribute('src', './Fokus-projeto/imagens/play_arrow.png');
     intervaloId = null;
 }
 
